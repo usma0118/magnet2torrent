f37b23d8c0f19b7984aeba8bf0c80d0bacec9fe1
Usman Masood <usmanmasood@msn.com>
2022-01-25 14:08:40 +0100

    minor updates

--- buid-docker.sh
+++ buid-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-DOCKER_BUILDKIT=1 docker build . -t antaresinc/magnet2torrent:1.0.0-alpha -t antaresinc/magnet2torrent:latest
+DOCKER_BUILDKIT=1 docker build . -t antaresinc/magnet2torrent:1.0.0 -t antaresinc/magnet2torrent:latest
 docker push antaresinc/magnet2torrent -a
