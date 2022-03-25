779c03b5ddf70027563dee60f4cb22eaa6c5a833
usma0118 <usmanmasood@msn.com>
2022-03-25 14:31:09 +0100

    transmissionclient added

--- torrent/transmissionclient.py
+++ torrent/transmissionclient.py
@@ -1,9 +1,10 @@
+from array import array
 from transmission_rpc import Client
 
 class transmission_client:
-    def __init__(self):
-        # self.logger=logger
-        self.client=Client(host='transmission.antaresinc.home', port=80,username='secrets', password='alpha123')
+    def __init__(self,logger,host,username,password,path="/transmission",port=9091):
+        self.logger=logger
+        self.client=Client(host=host,path=path, port=port,username=username, password=password)
     
     def get_torrents(self):
         return self.client.get_torrents()
@@ -11,19 +12,6 @@ class transmission_client:
     def get_torrent(self,torrent_id):
         return self.client.get_torrent(torrent_id)
 
-    def update_trackers(self,torrent_id,trackers):
+    def update_trackers(self,torrent_id:int,trackers:array):
         self.client.change_torrent(torrent_id,trackerAdd=trackers)
         self.client.reannounce_torrent(torrent_id)
-        return
-
-# if __name__ == '__main__':
-#     tr=transmission_client()
-#     t=tr.list()
-#     for tort in t:
-#         print(tort.name)
-#         print(tort.hashString)
-#         trackers=tort._fields.get("trackers")
-#         for t1 in trackers[0]:
-#             print(t1.get("announce"))
-#         tr.update_trackers(tort.id,["udp://open.tracker.cl:1337/announce"])
-#         print()
