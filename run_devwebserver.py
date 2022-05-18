cd1ca554ec54a8c42548ea56ae64a9aa9b9e454a
usma0118 <usmanmasood@msn.com>
2022-05-18 13:32:39 +0200

    Added support for .devcontainer
    Signed-off-by: usma0118 <usmanmasood@msn.com>

--- run_devwebserver.py
+++ run_devwebserver.py
@@ -4,15 +4,3 @@ import uuid
 app=create_app(str(uuid.uuid4()))
 app.config['TESTING'] = True
 app.run('127.0.0.1',port=8080,debug=True,load_dotenv=True)
-
-
-
-# if __name__ == '__main__':
-#     tr=transmission_client()
-#     t=tr.list()
-#     for tort in t:
-#         trackers=tort._fields.get("trackers")
-#         for t1 in trackers[0]:
-#             print(t1.get("announce"))
-#         tr.update_trackers(tort.id,["udp://open.tracker.cl:1337/announce"])
-#         print()
--- web/routes.py
+++ web/routes.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from urllib import request
 import os
-import os.path,time
+import os.path
+import time
 from decouple import config
 from flask import render_template
-from flask import Blueprint,redirect, url_for,request,flash
+from flask import Blueprint,redirect, url_for,flash
 from flask_login import login_required
 from torrents.clients import TransmissionClient
 
@@ -13,17 +14,23 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @login_required
 def index():
+    '''
+    Index method
+    '''
     folder_watch = os.path.join(config('magnet_watch',default='blackhole'))
 
     # Show directory contents
-    types = ['*.magnet*', '*.torrent*']
+    torrent_extensions = ['*.magnet*', '*.torrent*']
     magnets = []
-    for type in types:
-        files=Path(folder_watch).glob(type)
+    for torrent_extension in torrent_extensions:
+        files=Path(folder_watch).glob(torrent_extension)
         magnets += files
     files=[]
     for magnet in magnets:
-        files.append({'name':magnet.name,'magnet_url': magnet.read_text(),'created':time.ctime(os.path.getctime(magnet)),'modified':time.ctime(os.path.getmtime(magnet))})
+        if '.torrent' in magnet.name:
+            files.append({'name':magnet.name,'magnet_url': '', 'created':time.ctime(os.path.getctime(magnet)),'modified':time.ctime(os.path.getmtime(magnet))})
+        else:
+            files.append({'name':magnet.name,'magnet_url': magnet.read_text(),'created':time.ctime(os.path.getctime(magnet)),'modified':time.ctime(os.path.getmtime(magnet))})
 
     return render_template('index.html', files=files)
 
@@ -32,8 +39,8 @@ def index():
 def torrents():
     torrent_view=[]
     client=TransmissionClient(config('transmission_host'),config('transmission_user'),config('transmission_password'),port=config('transmission_port'),path=config('transmission_path',default='/transmission/rpc'))
-    torrents=client.get_torrents()
-    for trt in torrents:
+    client_torrents=client.get_torrents()
+    for trt in client_torrents:
         torrent_view.append({'id':trt.id,'name':trt.name,'status':trt.status,'progress': round(float(trt.progress)),'peers': trt.peers, 'stalled':trt.is_stalled,'size':trt.totalSize,'hash':trt.hashString,'magnet_url':trt.magnetLink,'isPrivate':trt.isPrivate})
 
     return render_template('torrents.html', torrents=torrent_view)
