5671d1bcc68d2aa6d673a5e92dd527fb04796545
usma0118 <usmanmasood@msn.com>
2022-06-14 13:34:49 +0200

    ui changes

--- run_devwebserver.py
+++ run_devwebserver.py
@@ -1,8 +1,9 @@
+from logging import Logger
 from web import create_app
 import uuid
 
-app=create_app(str(uuid.uuid4()))
-app.config['TESTING'] = True
-app.config['LOGIN_DISABLED'] = True
+app=create_app(str(uuid.uuid4()), Logger('devwebserver'))
+# app.config['TESTING'] = True
+# app.config['LOGIN_DISABLED'] = True
 app.login_manager.init_app(app)
 app.run('127.0.0.1',port=8080,debug=True,load_dotenv=True)
